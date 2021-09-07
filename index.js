b537fd20321033eb5fdd7d152eb51219a23cfb70
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-09-07 10:29:47 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.route'));
 app.use('/api/todo', require('./routes/todo.route'));
 app.use('/api/users', require('./routes/user.route'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 async function start() {
   try {
